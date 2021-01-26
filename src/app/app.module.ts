7fe47a5a92d80b1635f06fe84b84ffdcb2aeb359
Kai-Al <63424399+Kai-Al@users.noreply.github.com>
2021-01-25 19:40:06 -0500

    Enrutamiento de mello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -24,19 +25,20 @@ import { MatListModule } from '@angular/material/list';
     HeaderComponent,
     CreateTransactionComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatToolbarModule,
-        LayoutModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        MatListModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
